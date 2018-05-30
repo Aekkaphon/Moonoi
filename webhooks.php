814d940ec476687f8f91b164cfa979b8ed0451c6
comrmutto <comrmutto1990@gmail.com>
2018-05-30 11:09:22 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -9,13 +9,13 @@
     $arrayHeader[] = "Authorization: Bearer {$accessToken}";
     
     //รับข้อความจากผู้ใช้
-    $message = $arrayJson['events'][0]['message']['text']['text2'];
+    $message = $arrayJson['events'][0]['message']['text'];
 #ตัวอย่าง Message Type "Text"
     if($message == "สวัสดี"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "สวัสดีครับ";
-        $arrayPostData['messages'][0]['text2'] = "เราคือไอทีบอท";
+        $arrayPostData['messages'][0]['text'] = "เราคือไอทีบอท";
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Sticker"
